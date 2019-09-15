6b10c03957489f36a4b29ab3e42af90f4bea44df
Kirsti Norton <kirsti.norton@domo.com>
2019-09-15 15:38:37 -0600

    changing primary color to black

--- src/assets/jss/material-kit-react.js
+++ src/assets/jss/material-kit-react.js
@@ -69,7 +69,7 @@ const defaultFont = {
   lineHeight: "1.5em"
 };
 
-const primaryColor = "#9c27b0";
+const primaryColor = "#000000";
 const warningColor = "#ff9800";
 const dangerColor = "#f44336";
 const successColor = "#4caf50";
@@ -79,7 +79,7 @@ const grayColor = "#999999";
 
 const primaryBoxShadow = {
   boxShadow:
-    "0 12px 20px -10px rgba(156, 39, 176, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(156, 39, 176, 0.2)"
+    "0 12px 20px -10px rgba(0, 0, 0, 0.28), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(0, 0, 0, 0.2)"
 };
 const infoBoxShadow = {
   boxShadow:
