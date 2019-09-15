4ae906734f5ecdcbff06aa4679dd91322ead6812
Kirsti Norton <kirsti.norton@domo.com>
2019-09-15 12:37:38 -0600

    initial commit

---
+++ src/views/ContactPage/ContactPage.js
@@ -0,0 +1,48 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+
+// @material-ui/icons
+
+// core components
+import Header from "components/Header/Header.js";
+import Footer from "components/Footer/Footer.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import Button from "components/CustomButtons/Button.js";
+import HeaderLinks from "components/Header/HeaderLinks.js";
+import Parallax from "components/Parallax/Parallax.js";
+
+import styles from "assets/jss/material-kit-react/views/contactPage.js";
+
+// Sections for this page
+import WorkSection from "../LandingPage/Sections/WorkSection.js";
+
+const dashboardRoutes = [];
+
+const useStyles = makeStyles(styles);
+
+export default function ContactPage(props) {
+  const classes = useStyles();
+  const { ...rest } = props;
+  return (
+    <div>
+      <Header
+        color="white"
+        routes={dashboardRoutes}
+        brand="TR Luxury Travel"
+        rightLinks={<HeaderLinks />}
+        fixed
+        {...rest}
+      />
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <div className={classes.container}>
+          <WorkSection />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
+}
